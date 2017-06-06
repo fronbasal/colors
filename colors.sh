807a9c0edb274953479c2b7472b4b04f75d9452f
Fronbasal <mail@fronbasal.de>
2017-06-06 14:27:42 +0200

    Typo in echo

--- colors.sh
+++ colors.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'export PS1="\[\033[38;5;33m\][\[$(tput sgr0)\]\[\033[38;5;4m\]\A\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;39m\]\u\[$(tput sgr0)\]\[\033[38;5;42m\]@\[$(tput sgr0)\]\[\033[38;5;41m\]\h\[$(tput sgr0)\]\[\033[38;5;14m\]:\[$(tput sgr0)\]\[\033[38;5;61m\]\W\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;151m\]\\$\[$(tput sgr0)\]\[\033[38;5;33m\]]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"' >> ~/.bashrc
-print "Installed colors.\nRun source ~/.bashrc to apply colors."
+printf "Installed colors.\nRun source ~/.bashrc to apply colors."
 
